367c2f400f141056e5525be2be2edb172c66b205
Alanjid <carlosalanjg123@gmail.com>
2023-09-27 14:25:53 -0600

    main

--- Unidad 2/Lambda.lisp
+++ Unidad 2/Lambda.lisp
@@ -1,5 +1,12 @@
-(defun nombrefuncion (a b c))
-(lambda (a b c) (+ a (* b c)))
-(defvar *foo* (lambda (a b c) (+ a (* b c)))
-(funcall *foo* 1 2 3)
+(defvar *cuadrado* (lambda a b) (* a b))
 
+
+
+(defun areaRectangulo ()
+  (princ "Dame la altura del rectangulo:  ")
+  (setq num1 (read))
+  (princ "Dame la base del rectangulo:  ")
+  (setq num2 (read))
+  ;(* num1 num2)
+  (format t " la salida es ~A" (funcall *cuadrado* num1 num2))
+)
