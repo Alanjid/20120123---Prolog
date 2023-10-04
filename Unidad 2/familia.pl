2a38f6d08eb3d11a5cce381d58e83b86070f4de6
Alanjid <carlosalanjg123@gmail.com>
2023-10-04 12:04:07 -0600

    main

--- Unidad 2/familia.pl
+++ Unidad 2/familia.pl
@@ -1,5 +1,11 @@
 padre(juan).
+padre(antonio).
+padre(luiso).
+padre(eduardo).
+padre(pepe).
 madre(luisa).
+padrede(pedro, juan).
 padrede(juan, julio).
 padrede(juan, jose).
 madrede(luisa, maria).
+abuelo(X,Y):- padrede(X,W),padrede(W,Y).
